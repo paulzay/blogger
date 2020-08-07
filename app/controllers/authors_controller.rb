b31e5552393c10f2bded0c875354976b8111b0f1
DickensJuma <dickensjuma13@gmail.com>
2020-08-07 18:53:17 +0300

    update the application

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,7 +1,7 @@
 class AuthorsController < ApplicationController
+
   before_action :set_author, only: [:show, :edit, :update, :destroy]
 
-  before_filter :zero_authors_or_authenticated, only: [:new, :create]
 
 def zero_authors_or_authenticated
   unless Author.count == 0 || current_user
